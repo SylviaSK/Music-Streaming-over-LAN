c0cfdd67eb39a17297f66fabdf3c35edb5907566
Sylvia-Krech <40416079+Sylvia-Krech@users.noreply.github.com>
2022-05-23 06:58:46 -0500

    Updated server to work with POST rather than hijacking GET

--- add_playlist.py
+++ add_playlist.py
@@ -2,60 +2,52 @@ import sys
 import fileinput
 
 #declare vars
-FILEPATH = "/home/pi/Public/server/stream.html"
 playlistName = None
-#a big hardcoded block that is the basis for this 'form filling'
-theBigList = ["space",
-			  "<a href=\"/playlist.",
-			  playlistName,
-			  "\">\n",
-			  "space",
-			  "  <div class=\"playlist-container\">\n",
-			  "space",
-			  "    <img src=\"/resources/playlists/",
-			  playlistName,
-			  ".png\" alt=\"",
-			  playlistName, 
-			  " playlist image\" class=\"playlist-image\">\n",
-			  "space",
-			  "    <div class=\"playlist-overlay\"></div>\n",
-			  "space",
-			  "    <div class=\"playlist-text\">",
-			  playlistName,
-			  "</div>\n",
-			  "space",
-			  "  </div>\n",
-			  "space",
-			  "</a>\n",
-			  "<!-- PYTHON SCRIPT MARKER-->"]
+# commandline arguements:
+# 1st arguement: filepath to file to change
+# 2nd arguement and beyond: name of playlists to add
 
 def main():
-	spacingStr = "" #it doesn't like being at the top and idk why
-	#grab playlist name to add
-	playlistName = sys.argv[1]
-	
-	#search through file for marker
-	with fileinput.FileInput(FILEPATH, inplace = True, backup ='.bak') as f:
-		for line in f:
-			if "<!-- PYTHON SCRIPT MARKER-->" in line:
-				pass
-				#add proper spacing to the strings
-				for i in range(len(theBigList)):
-					if theBigList[i] == "space":
-						theBigList[i] = spacingStr
-				#replace the blanks with the playlist name
-				for i in range(len(theBigList)):
-					if theBigList[i] == None:
-						theBigList[i] = playlistName
-				output = "".join(theBigList)
-				print(line.replace("<!-- PYTHON SCRIPT MARKER-->", output), end='')
-			else:
-				spacingStr = " "*max(len(line)-5, 0)
-				print(line, end='')
-	eprint(playlistName,"successfully added.")
+    spacingStr = "" #it doesn't like being at the top and idk why
+    #grab playlist name to add
+
+    try:
+        FILEPATH = sys.argv[1]
+    except:
+        FILEPATH = "server/test.html"
+        
+    playlistNames = sys.argv[2:]
+    if playlistNames == []:
+        playlistNames = ["TESTING"]
+    
+    
+    #get base string to drop in
+    baseString = []
+    with fileinput.FileInput("add_playlist_form.html") as f:
+        for line in f:
+            baseString.append(line)
+    baseString = "".join(baseString)
+
+    #search through file for marker
+    with fileinput.FileInput(FILEPATH, inplace = True, backup ='.bak') as f:
+        for line in f:
+            if "<!-- PYTHON SCRIPT MARKER-->" in line:
+                output = ""
+                for name in playlistNames:
+                    temp = baseString.replace("INPUT", name) # fill in name
+                    # TODO deal with indentation even though it isnt 100% nessecary:
+                    output += temp
+                output += line #python script marker
+                print(output[:-1], end='')
+            else:
+                print(line, end='')
+    eprint(playlistNames,"successfully added.")
+       
+#def get
+                                 
 #for debug
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 if __name__=="__main__":
-	main()
+    main()
--- server.py
+++ server.py
@@ -1,83 +1,128 @@
 # server.py
+# Note: This requires xdotool and vlc installed
 import http.server # Our http server handler for http requests
 import socketserver # Establish the TCP Socket connections
 import subprocess
 import copy #reduce to j deepcopy if i remember
 import sys
+import cgi
+from collections import namedtuple
 
 #dummy class to enable .kill() to be called on currentStream before it
 #gets assigned to a real subprocess
 class killBox():
     def kill():
         return
-
+## Initialize from config
+#with open config
+debug = True
 PORT = 8000
-specifiedPaths = ("resources/icon.png")
-errorMessagePage = None
+errorMessagePage = "server/resources/error.html"
 #use cvlc to remove interface, vlc to keep
-templatePlaylistArgs = ['vlc', '/home/pi/Public/server/resources/playlists/', '--sout=#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:duplicate{dst=http{dst=:8080/stream.mp3},dst=display}', '--sout-keep', '-L']
+templatePlaylistArgs = ['vlc', 'server/resources/playlists/', '--sout=#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:duplicate{dst=http{dst=:8080/stream.mp3},dst=display}', '--sout-keep', '-L']
 nextSongCommand = ['xdotool', 'key', 'alt+l', 'key', 'x']
 prevSongCommand = ['xdotool', 'key', 'alt+l', 'key', 'v']
+
+ResponseStatus = namedtuple("HTTPStatus", ["code", "message"])
+NO_CONTENT = ResponseStatus(code=204, message="No Content")
 currentStream = killBox
 temporarySubprocess = killBox
 
-with open("/home/pi/Public/server/resources/error.html") as file:
+with open(errorMessagePage) as file:
     errorMessagePage = file.read()
 
 
-def handleButtonLinks(callingObject):
-    if "playlist." in callingObject.path:
-        playPlaylist(callingObject)
-    elif "nextSong" in callingObject.path:
-        temporaryCommandlineCall(callingObject, nextSongCommand)
-        eprint("fastForwarding")
-    elif "prevSong" in callingObject.path:
-        temporaryCommandlineCall(callingObject, prevSongCommand)
-        eprint("prevSong")
         
-def playPlaylist(callingObject):
+def playPlaylist(playlist):
     global currentStream
-    print("current path:", callingObject.path)
     currentStream.kill()
     temp = copy.deepcopy(templatePlaylistArgs)
-    temp[1] += callingObject.path[10:].replace("%20", " ") + ".xspf"
-    print("Swapping to playlist", callingObject.path[10:])
+    temp[1] += playlist + ".xspf" #callingObject.path[10:].replace("%20", " ") 
+    print("Swapping to playlist", playlist)
     currentStream = subprocess.Popen(temp)
-    callingObject.path = "/stream.html"
     
-#for use in fastForward/backup commands
-def temporaryCommandlineCall(callingObject, command):
+def handleControls(controls):
+    if controls == "nextSong" :
+            temporaryCommandlineCall(nextSongCommand)
+            eprint("Skipping current Song")
+    elif controls == "prevSong":
+        temporaryCommandlineCall(prevSongCommand)
+        eprint("Going back to previous Song")
+
+# creates a commandline call that will be killed when this function is called again.
+# TODO: aen improvement would involve making this spin off a new thread/child and just killing it as soon as it completes
+def temporaryCommandlineCall(command):
     global temporarySubprocess
     temporarySubprocess.kill()
     temporarySubprocess = subprocess.Popen(command)
-    callingObject.path = "/stream.html"  
     
 
 
 class httpRequestHandler(http.server.SimpleHTTPRequestHandler):
+            
     def do_GET(self):
         if self.path == "/":
             self.path = "/stream.html"
             
-        #handle buttons
-        handleButtonLinks(self)
-            
         self.path = "/server" + self.path
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
  
-    def send_error(self, code, message=None):
+    def do_POST(self):
+        #self.printContents()
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST', 'CONTENT_TYPE':self.headers['Content-Type']}
+        )
         
+        # pull up the playlist
+        playlistName = form.getvalue("playlistName")
+        print(playlistName)
+        if playlistName != None:
+            playPlaylist(playlistName)
+            eprint("Playing:", playlistName)
+            self.send_response(NO_CONTENT.code, NO_CONTENT.message)
+            self.end_headers()
+            return
+        
+        # song controls
+        controls = form.getvalue("controls")
+        print(controls)
+        if controls != None:
+            handleControls(controls)
+            self.send_response(NO_CONTENT.code, NO_CONTENT.message)
+            self.end_headers()
+            return
+        
+    def send_error(self, code, message=None):
         handleButtonLinks(self)
             
         self.error_message_format = errorMessagePage
         http.server.SimpleHTTPRequestHandler.send_error(self, code, message)
-        
+
+    #prints every bit of info I know that a SimpleHTTPRequestHandler has
+    def printContents(self):
+        print("client_address:", self.client_address)
+        print("close_connection:", self.close_connection)
+        print("requestline:", self.requestline)
+        print("command:", self.command)
+        print("path:", self.path)
+        print("request_version:", self.request_version)
+        print("headers:", self.headers)
+        print("rfile:", self.rfile)
+        print("wfile:", self.wfile)
+
+
 def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
-        
-handler = httpRequestHandler
- 
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print("Http Server Serving at port", PORT)
-    httpd.serve_forever()
+    if debug:
+        print(*args, file=sys.stderr, **kwargs)
+
+
+if __name__ == "__main__":
+    handler = httpRequestHandler
+     
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print("Http Server Serving at port", PORT)
+        httpd.serve_forever()
     
+
